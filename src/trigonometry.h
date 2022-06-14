201e5ff9c1921670fea320bfab321eb263249e6b
amartinsmg <andremindurimg@gmail.com>
2022-06-13 21:48:36 -0300

    trygonometry update

--- src/trigonometry.h
+++ src/trigonometry.h
@@ -1,13 +1,37 @@
 #include <math.h>
 
-double findSideByPythagorean(double sideA, double sideB)
+double hypotenusePythagorean(double sideA, double sideB)
 {
   double result = sqrt(pow(sideA, 2) + pow(sideB, 2));
   return result;
 }
 
-double findSideByLawOfCos(double sideA, double sideB, double angleGama)
+double sideByPythagorean(double hypotenuse, double sideB)
 {
-  double result = sqrt(pow(sideA, 2) + pow(sideB, 2) - (2 * sideA * sideB * cos(angleGama)));
+  double result = sqrt(pow(hypotenuse, 2) - pow(sideB, 2));
+  return result;
+}
+
+double sideByLawOfCos(double sideA, double sideB, double oppositeAngle)
+{
+  double result = sqrt(pow(sideA, 2) + pow(sideB, 2) - (2 * sideA * sideB * cos(oppositeAngle)));
+  return result;
+}
+
+double angleByLawOfCos(double oppositeSide, double sideA, double sideB)
+{
+  double result = acos(pow(sideA, 2) + pow(sideB, 2) - pow(oppositeSide, 2) / (2 * sideA * sideB));
+  return result;
+}
+
+double sideBySinOfCos(double oppositeAngle, double sideA, double oppositeAngle2A)
+{
+  double result = sideA / sin(oppositeAngle2A) * sin(oppositeAngle);
+  return result;
+}
+
+double angleBySinOfCos(double oppositeSide, double sideA, double oppositeAngle2A)
+{
+  double result = asin(oppositeSide / (sideA / sin(oppositeAngle2A)));
   return result;
 }
