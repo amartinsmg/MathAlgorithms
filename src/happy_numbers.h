fb3818453311a689bb69503bb509034caeb6f665
amartinsmg <andremindurimg@gmail.com>
2022-09-20 20:45:41 -0300

    Update happy_numbers.h

--- src/happy_numbers.h
+++ src/happy_numbers.h
@@ -1,28 +1,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int _sumOfSquareEachDig(long long num)
-{
-  long long sum = 0;
-  int remainder;
-  while (num > 0)
-  {
-    remainder = num % 10;
-    num /= 10;
-    sum += remainder * remainder;
-  }
-  return sum;
-}
-
 bool isHappy(long long num)
 {
   long long sum, *numSet = (long long *)malloc(sizeof(*numSet));
-  int i, lenNumSet;
+  int i, lenNumSet, remainder;
   numSet[0] = num;
   lenNumSet = 1;
   while (true)
   {
-    sum = _sumOfSquareEachDig(num);
+    sum = 0;
+    while (num > 0)
+    {
+      remainder = num % 10;
+      num /= 10;
+      sum += remainder * remainder;
+    }
     if (sum == 1)
       return true;
     for (i = 0; i < lenNumSet; i++)
