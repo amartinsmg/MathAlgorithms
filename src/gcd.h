3574d71fa09da5105c962e9df13817bb2e1c5196
andre <andremindurimg@gmail.com>
2023-06-30 20:56:33 -0300

    Update gcd.h

--- src/gcd.h
+++ src/gcd.h
@@ -1,5 +1,13 @@
 #include <math.h>
 
+/**
+  @brief Calculate the greatest common divisor (GCD) of two integers.
+  @param x The first integer.
+  @param y The second integer.
+  @return The GCD of 'x' and 'y'.
+  @pre x and y cannot both be 0.
+*/
+
 int gcd(int x, int y)
 {
   int result, remainder;
