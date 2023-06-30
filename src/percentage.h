0acac90322cca2f38c3b465c3570a2b5fde8deaf
andre <andremindurimg@gmail.com>
2023-06-30 20:58:08 -0300

    Update percentage.h

--- src/percentage.h
+++ src/percentage.h
@@ -1,10 +1,24 @@
 #include <assert.h>
 
+/**
+  @brief Calculate n percent of x.
+  @param x The value.
+  @param n The percentage.
+  @return The calculated result.
+*/
+
 double nPercentOfX(double x, double n)
 {
   double result = x / 100 * n;
   return result;
 }
+
+/**
+  @brief Calculate what percentage n is of x.
+  @param x The value.
+  @param n The value to calculate the percentage for.
+  @return The calculated percentage.
+*/
 
 double nIsWhatPercentOfX(double x, double n)
 {
