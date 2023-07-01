6467a57702a1b4bc355284c5c3719c1a8d60f330
andre <andremindurimg@gmail.com>
2023-07-01 08:52:22 -0300

    Update area.h

--- src/area.h
+++ src/area.h
@@ -221,6 +221,12 @@ double ellipseArea(double semiMajorAxis, double semiMinorAxis)
   return result;
 }
 
+/**
+  @brief Calculates the surface area of a cube.
+  @param side The length of the side of the cube.
+  @return The surface area of the cube.
+*/
+
 double cubeArea(double side)
 {
   double result;
@@ -228,6 +234,14 @@ double cubeArea(double side)
   result = 6 * pow(side, 2);
   return result;
 }
+
+/**
+  @brief Calculates the surface area of a cuboid.
+  @param width The width of the cuboid.
+  @param length The length of the cuboid.
+  @param height The height of the cuboid.
+  @return The surface area of the cuboid.
+*/
 
 double cuboidArea(double width, double length, double height)
 {
@@ -237,6 +251,14 @@ double cuboidArea(double width, double length, double height)
   return result;
 }
 
+/**
+  @brief Calculates the surface area of a prism.
+  @param baseArea The area of the prism's base.
+  @param basePerimeter The perimeter of the prism's base.
+  @param height The height of the prism.
+  @return The surface area of the prism.
+*/
+
 double prismArea(double baseArea, double basePerimeter, double height)
 {
   double result;
@@ -245,12 +267,28 @@ double prismArea(double baseArea, double basePerimeter, double height)
   return result;
 }
 
+/**
+  @brief Calculates the surface area of a regular prism.
+  @param baseSide The length of the base side of the prism.
+  @param nOfBaseSides The number of sides in the base of the prism.
+  @param height The height of the prism.
+  @return The surface area of the prism.
+*/
+
 double regularPrismArea(double baseSide, int nOfBaseSides, double height)
 {
   double baseArea = regularPolygonArea(baseSide, nOfBaseSides),
          basePerimeter = baseSide * nOfBaseSides;
   return prismArea(baseArea, basePerimeter, height);
 }
+
+/**
+  @brief Calculates the surface area of a pyramid.
+  @param baseArea The area of the pyramid's base.
+  @param basePerimeter The perimeter of the pyramid's base.
+  @param slantHeight The slant height of the pyramid.
+  @return The surface area of the pyramid.
+*/
 
 double pyramidArea(double baseArea, double basePerimeter, double slantHeight)
 {
@@ -259,6 +297,14 @@ double pyramidArea(double baseArea, double basePerimeter, double slantHeight)
   result = baseArea + basePerimeter * slantHeight / 2;
   return result;
 }
+
+/**
+  @brief Calculates the surface area of a regular pyramid.
+  @param baseSide The length of the base side of the pyramid.
+  @param nOfBaseSides The number of sides in the base of the pyramid.
+  @param height The height of the pyramid.
+  @return The surface area of the pyramid.
+*/
 
 double regularPyramidArea(double baseSide, int nOfBaseSides, double heigth)
 {
@@ -269,6 +315,13 @@ double regularPyramidArea(double baseSide, int nOfBaseSides, double heigth)
   return pyramidArea(baseArea, basePerimeter, slantHeight);
 }
 
+/**
+  @brief Calculates the surface area of a cylinder.
+  @param baseRadius The radius of the cylinder's base.
+  @param height The height of the cylinder.
+  @return The surface area of the cylinder.
+*/
+
 double cylinderArea(double baseRadius, double height)
 {
   double result;
@@ -277,6 +330,13 @@ double cylinderArea(double baseRadius, double height)
   return result;
 }
 
+/**
+  @brief Calculates the surface area of a cone.
+  @param baseRadius The radius of the cone's base.
+  @param height The height of the cone.
+  @return The surface area of the cone.
+*/
+
 double coneArea(double baseRadius, double height)
 {
   double result;
@@ -284,6 +344,12 @@ double coneArea(double baseRadius, double height)
   result = circleArea(baseRadius) + M_PI * baseRadius * hypotenuse(height, baseRadius);
   return result;
 }
+
+/**
+  @brief Calculates the surface area of a sphere.
+  @param radius The radius of the sphere.
+  @return The surface area of the sphere.
+*/
 
 double sphereArea(double radius)
 {
