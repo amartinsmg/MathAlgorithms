c4d333d545a6112bd1f4452478859fbadfe1c148
andre <andremindurimg@gmail.com>
2023-07-04 17:56:04 -0300

    Update prime_factorization.h

--- src/prime_factorization.h
+++ src/prime_factorization.h
@@ -8,19 +8,16 @@
 
 long long *primeFactors(long long num)
 {
-  long long i, *factors = (long long *)calloc(64, sizeof(*factors)),
-               length = 0;
+  long long i,
+      *factors = (long long *)calloc(64, sizeof(*factors)),
+      length = 0;
   while (num > 1)
-  {
     for (i = 2; i <= num; i++)
-    {
       if (!(num % i))
       {
         factors[length++] = i;
         num /= i;
         break;
       }
-    }
-  }
   return factors;
 }
