902689dd4ae21011cb97ce395ebbac1906ccd650
andre <andremindurimg@gmail.com>
2023-07-04 17:45:35 -0300

    Update probability_distribution.h

--- src/probability_distribution.h
+++ src/probability_distribution.h
@@ -5,53 +5,54 @@
 /**
   @brief Calculate the binomial coefficient.
   @param trials The number of trials.
-  @param success_prob The success probability.
+  @param successProb The success probability.
   @param success The number of successful outcomes.
   @return The binomial coefficient.
   @pre trials must be greater than 0, success must be greater than or equal to 0,
-  success_prob must be between 0 and 1 (inclusive).
+ successProb must be between 0 and 1 (inclusive).
 */
 
-double binominal(int trials, double success_prob, int success)
+double binominal(int trials, double successProb, int success)
 {
   double result;
-  assert(trials > 0 && success >= 0 && success_prob >= 0 && success_prob <= 1);
+  assert(trials > 0 && success >= 0 && successProb >= 0 && successProb <= 1);
   result = combinationlf(trials, success) *
-           pow(success_prob, success) *
-           pow((1 - success_prob), (trials - success));
+           pow(successProb, success) *
+           pow((1 - successProb), (trials - success));
   return result;
 }
 
 /**
   @brief Calculate the Poisson probability.
-  @param mean The mean value.
+  @param lambda The mean value.
   @param x The number of occurrences.
   @return The Poisson probability.
 */
 
-double poisson(double mean, unsigned x)
+double poisson(double lambda, unsigned x)
 {
   double result;
-  result = exp(-mean) * pow(mean, x) / factoriallf(x);
+  result = exp(-lambda) * pow(lambda, x) / factoriallf(x);
   return result;
 }
 
 /**
   @brief Calculate the cumulative distribution function (CDF) of a Gaussian distribution.
-  @param mean The mean value of the Gaussian distribution.
+  @param mu The mean value of the Gaussian distribution.
   @param standardDev The standard deviation of the Gaussian distribution.
   @param x The input value.
   @return The cumulative distribution function (CDF) value.
   @pre standardDev must be greater than 0.
 */
 
-double gaussianCDF(double mean, double standadDev, double x)
+double gaussianCDF(double mu, double standadDev, double x)
 {
-  double phi, result, z, denominator = 1,
-                         sum = 0;
+  double phi, result, z,
+      denominator = 1,
+      sum = 0;
   int i;
   assert(standadDev > 0);
-  z = (x - mean) / standadDev;
+  z = (x - mu) / standadDev;
   phi = exp(-pow(z, 2) / 2) / sqrt(2 * M_PI);
   for (i = 1; i <= 100; i += 2)
   {
