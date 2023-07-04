f3c1c2a34601dbb323fa23128503a2878f834e28
andre <andremindurimg@gmail.com>
2023-07-04 17:52:39 -0300

    Update perfect_numbers.h

--- src/perfect_numbers.h
+++ src/perfect_numbers.h
@@ -11,10 +11,8 @@ bool isPerfect(long long num)
   long long sum = 0;
   int i;
   for (i = 1; i < num; i++)
-  {
     if (num % i == 0)
       sum += i;
-  }
   if (sum == num)
     return true;
   return false;
