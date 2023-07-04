1f4d0783b3d358c86c8a19df106071b830311968
andre <andremindurimg@gmail.com>
2023-07-04 17:51:10 -0300

    Update prime_numbers.h

--- src/prime_numbers.h
+++ src/prime_numbers.h
@@ -8,19 +8,11 @@
 
 bool isPrime(long long num)
 {
-  int i = 3;
+  int i;
   if (!(num % 2))
-  {
-    if (num == 2)
-      return true;
-    else
-      return false;
-  }
-  while (num % i && i <= num)
-  {
+    return num == 2 ? true : false;
+  for (i = 3; num % i && i <= num; i += 2)
     if (i * i > num)
       return true;
-    i += 2;
-  }
   return false;
 }
