862f8b4ff336948b7c47d6d8fe28b6f3963e57c8
andre <andremindurimg@gmail.com>
2023-07-06 17:00:05 -0300

    Update sort.h

--- src/include/sort.h
+++ src/include/sort.h
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 
+#ifndef SORT_H
+#define SORT_H
+
 /**
   @brief Sorts an array of doubles in ascending order.
   This function takes an array of doubles and sorts it in ascending order using the merge sort algorithm.
@@ -8,14 +11,14 @@
   @return A pointer to the sorted array of doubles.
 */
 
-double *sort(double *arr, int length)
+double *sort(double *arr, unsigned length)
 {
   double *buffer1 = (double *)malloc(sizeof(*buffer1) * length),
          *buffer2 = (double *)malloc(sizeof(*buffer2) * length),
          *src, *target, *left, *right, *result;
-  int i, j, k, half, targetIndex, leftIndex, rightIndex, log2_length, pow2_i;
+  unsigned i, j, k, half, targetIndex, leftIndex, rightIndex, log2_length, pow2_i;
   assert(length > 0);
-  log2_length = (int)ceil(log2((double)length));
+  log2_length = (unsigned)ceil(log2((double)length));
   for (i = 0; i < length; i++)
     buffer1[i] = arr[i];
   for (i = 1; i <= log2_length; i++)
@@ -23,25 +26,24 @@ double *sort(double *arr, int length)
     src = i % 2 ? buffer1 : buffer2,
     target = !(i % 2) ? buffer1 : buffer2;
     targetIndex = 0;
-    pow2_i = (int)pow(2, (double)i);
+    pow2_i = (unsigned)pow(2, (double)i);
     half = pow2_i / 2;
     for (j = 0; j < length; j += pow2_i)
     {
       left = (double *)(src + j);
       right = (double *)(left + half);
       leftIndex = rightIndex = 0;
-      k = -1;
-      while (++k < pow2_i && targetIndex < length)
-      {
+      for (k = 0; k < pow2_i && targetIndex < length; k++)
         if (leftIndex >= half)
           target[targetIndex++] = right[rightIndex++];
         else if (rightIndex >= half || j + half + rightIndex >= length)
           target[targetIndex++] = left[leftIndex++];
         else
           target[targetIndex++] = left[leftIndex] < right[rightIndex] ? left[leftIndex++] : right[rightIndex++];
-      }
     }
   }
   result = log2_length % 2 ? buffer2 : buffer1;
   return result;
 }
+
+#endif /* SORT_H */
