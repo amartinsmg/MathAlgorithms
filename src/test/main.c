3b83c13c594432241fe568ceb88b8e597ef140b3
andre <andremindurimg@gmail.com>
2023-07-07 18:16:54 -0300

    Update test

--- src/test/main.c
+++ src/test/main.c
@@ -3,14 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <armstrong_numbers.h>
+#include <basic_operations.h>
+#include <combinatorics.h>
+#include <factorial.h>
+#include <gcd.h>
 #include <geometry.h>
 #include <growth.h>
 #include <happy_numbers.h>
 #include <lcm.h>
+#include <percentage.h>
 #include <perfect_numbers.h>
 #include <prime_factorization.h>
 #include <prime_numbers.h>
 #include <probability_distribution.h>
+#include <statistics.h>
+#include <trigonometry.h>
 #include "include/array_cmp.h"
 #include "include/test.h"
 
