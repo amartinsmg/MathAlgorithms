a31c1fe57f3c92e3497e52da7a4241c89f1bcb1c
André Martins <andremindurimg@gmail.com>
2026-02-24 20:31:39 -0300

    Refactor lcm.h: Improve documentation formatting and remove redundant assertions

--- src/lcm.h
+++ src/lcm.h
@@ -1,22 +1,24 @@
-#include <assert.h>
-#include <math.h>
-#include "gcd.h"
-
 #ifndef LCM_H
 #define LCM_H
 
+#include <errno.h>
+#include <math.h>
+#include "gcd.h"
+
 /**
-  @brief Calculate the least common multiple (LCM) of two integers.
-  @param x The first integer.
-  @param y The second integer.
-  @return The LCM of 'x' and 'y'.
-  @pre x and y cannot both be 0.
-*/
+ * @brief Calculate the least common multiple (LCM) of two integers.
+ *
+ * @param x The first integer.
+ * @param y The second integer.
+ *
+ * @return The LCM of 'x' and 'y'.
+ */
 
 long long lcm(int x, int y)
 {
+  if (x == 0 || y == 0)
+    return 0;
   long long result;
-  assert(x != 0 && y != 0);
   result = abs(x * y / gcd(x, y));
   return result;
 }
