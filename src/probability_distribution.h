cb5507dc3f1917dd8dbe3b6058e18e429bf8d966
André Martins <andremindurimg@gmail.com>
2026-02-24 20:50:41 -0300

    Refactor probability_distribution.h: Improve documentation formatting and remove redundant assertions

--- src/probability_distribution.h
+++ src/probability_distribution.h
@@ -1,7 +1,3 @@
-#include <assert.h>
-#include <math.h>
-#include "combinatorics.h"
-
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif /* M_PI */
@@ -9,21 +5,25 @@
 #ifndef PROBABILITY_DISTRIBUTION_H
 #define PROBABILITY_DISTRIBUTION_H
 
+#include <math.h>
+#include "combinatorics.h"
+
 /**
-  @brief Calculate the binomial coefficient.
-  @param trials The number of trials.
-  @param successProb The success probability.
-  @param success The number of successful outcomes.
-  @return The binomial coefficient.
-  @pre trials must be greater than 0, success must be greater than or equal to 0,
- successProb must be between 0 and 1 (inclusive).
-*/
+ * @brief Calculate the binomial coefficient.
+ *
+ * @param trials The number of trials.
+ * @param successProb The success probability.
+ * @param success The number of successful outcomes.
+ *
+ * @return The binomial coefficient.
+ */
 
 double binominal(int trials, double successProb, int success)
 {
+  if (trials < 0 || success < 0 || success > trials ||
+      successProb < 0 || successProb > 1)
+    return NAN;
   double result;
-  assert(trials > 0 && success >= 0 && success <= trials &&
-         successProb >= 0 && successProb <= 1);
   result = combinationlf(trials, success) *
            pow(successProb, success) *
            pow((1 - successProb), (trials - success));
@@ -31,36 +31,43 @@ double binominal(int trials, double successProb, int success)
 }
 
 /**
-  @brief Calculate the Poisson probability.
-  @param lambda The mean value.
-  @param x The number of occurrences.
-  @return The Poisson probability.
-*/
+ * @brief Calculate the Poisson probability.
+ *
+ * @param lambda The mean value.
+ * @param x The number of occurrences.
+ *
+ * @return The Poisson probability.
+ */
 
 double poisson(double lambda, unsigned x)
 {
+  if (lambda < 0)
+    return NAN;
   double result;
-  assert(lambda >= 0);
   result = exp(-lambda) * pow(lambda, x) / factoriallf(x);
   return result;
 }
 
 /**
-  @brief Calculate the cumulative distribution function (CDF) of a Gaussian distribution.
-  @param mu The mean value of the Gaussian distribution.
-  @param standardDev The standard deviation of the Gaussian distribution.
-  @param x The input value.
-  @return The cumulative distribution function (CDF) value.
-  @pre standardDev must be greater than 0.
-*/
+ * @brief Calculate the cumulative distribution function (CDF) of a Gaussian distribution.
+ *
+ * @param mu The mean value of the Gaussian distribution.
+ * @param standardDev The standard deviation of the Gaussian distribution.
+ * @param x The input value.
+ *
+ * @return The cumulative distribution function (CDF) value.
+ */
 
 double gaussianCDF(double mu, double standadDev, double x)
 {
+  if (standadDev < 0)
+    return NAN;
+  if (standadDev == 0)
+    return (x < mu) ? 0.0 : 1.0;
   double phi, result, z,
       denominator = 1,
       sum = 0;
   int i;
-  assert(standadDev > 0);
   z = (x - mu) / standadDev;
   phi = exp(-pow(z, 2) / 2) / sqrt(2 * M_PI);
   for (i = 1; i <= 100; i += 2)
