8c5e246863a8dd10f166fdf95a52ccf1d61336f3
André Martins <andremindurimg@gmail.com>
2026-02-24 17:14:06 -0300

    Refactor factorial calculation functions to replace assertions with error handling and improve documentation formatting

--- src/factorial.h
+++ src/factorial.h
@@ -4,36 +4,39 @@
 #define FACTORIAL_H
 
 /**
-  @brief Calculate the factorial of a number.
-  @param num The number for which factorial is to be calculated.
-  @return The factorial of the number.
-  @pre num must be a non-negative number.
-  @note This function is suitable for numbers up to 20. For numbers > 20 use the functions factoriallf() that use double precision
+ * @brief Calculate the factorial of a number.
+ * 
+ * @param num The number for which factorial is to be calculated.
+ * 
+ * @return The factorial of the number.
+ * 
+ * @note This function is suitable for numbers up to 20. For numbers > 20 use the functions factoriallf() that use double precision
   float to store the result, allowing numbers grether than 9223372036854776000 to be represented using exponential form.
  */
 
 long long factorial(int num)
 {
+  if(num < 0) return NAN;
   long long result = 1;
   int i;
-  assert(num >= 0);
   for (i = 1; i <= num; i++)
     result *= i;
   return result;
 }
 
 /**
-  @brief Calculate the factorial of a number using double precision.
-  @param num The number for which factorial is to be calculated.
-  @return The factorial of the number.
-  @pre num must be a non-negative number (double precision).
+ * @brief Calculate the factorial of a number using double precision.
+ * 
+ * @param num The number for which factorial is to be calculated.
+ * 
+ * @return The factorial of the number.
  */
 
 double factoriallf(int num)
 {
+  if(num < 0) return NAN;
   double result = 1;
   int i;
-  assert(num >= 0);
   for (i = 1; i <= num; i++)
     result *= i;
   return result;
