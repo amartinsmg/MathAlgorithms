c42187d93d2211b499a699ba0c1157133bfb4df6
André Martins <andremindurimg@gmail.com>
2026-02-24 20:39:22 -0300

    Refactor prime_factorization.h: Improve documentation formatting and remove redundant assertions

--- src/prime_factorization.h
+++ src/prime_factorization.h
@@ -1,21 +1,25 @@
-#include <assert.h>
-#include <stdlib.h>
-
 #ifndef PRIME_FACTORIZATION_H
 #define PRIME_FACTORIZATION_H
 
+#include <stdlib.h>
+
 /**
-  @brief Calculate the prime factors of a given number.
-  @param num The number to calculate prime factors for.
-  @return An array of prime factors.
-*/
+ * @brief Calculate the prime factors of a given number.
+ *
+ * @param num The number to calculate prime factors for.
+ *
+ * @return An array of prime factors.
+ *
+ * @note It is the caller's responsibility to free the memory allocated for the array.
+ */
 
 long long *primeFactors(long long num)
 {
+  if (num <= 1)
+    return NULL;
   long long i,
       *factors = (long long *)calloc(64, sizeof(*factors)),
       length = 0;
-  assert(num > 1);
   while (num > 1)
     for (i = 2; i <= num; i++)
       if (!(num % i))
