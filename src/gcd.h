6064966102153e4c6ebc3ae95a308e7c75b17519
André Martins <andremindurimg@gmail.com>
2026-02-24 20:08:18 -0300

    Reorganize includes and improve documentation formatting in gcd.h

--- src/gcd.h
+++ src/gcd.h
@@ -1,14 +1,16 @@
-#include <math.h>
-
 #ifndef GCD_H
 #define GCD_H
 
+#include <math.h>
+
 /**
-  @brief Calculate the greatest common divisor (GCD) of two integers.
-  @param x The first integer.
-  @param y The second integer.
-  @return The GCD of 'x' and 'y'.
-  @pre x and y cannot both be 0.
+ * @brief Calculate the greatest common divisor (GCD) of two integers.
+ * 
+ * @param x The first integer.
+ * @param y The second integer.
+ * @return The GCD of 'x' and 'y'.
+ * 
+ * @pre x and y cannot both be 0.
 */
 
 int gcd(int x, int y)
@@ -16,7 +18,7 @@ int gcd(int x, int y)
   int result, remainder;
   if (x == 0)
     return abs(y);
-  else if (y == 0)
+  if (y == 0)
     return abs(x);
   remainder = x % y;
   while (remainder)
