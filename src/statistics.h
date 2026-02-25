0f106bec1219fbb30bee2bdef063274394687baa
André Martins <andremindurimg@gmail.com>
2026-02-24 21:05:46 -0300

    Refactor statistics.h: Improve documentation formatting and remove redundant assertions

--- src/statistics.h
+++ src/statistics.h
@@ -1,25 +1,27 @@
-#include <assert.h>
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
 #include <math.h>
 #include "include/sort.h"
 #include "percentage.h"
 #include "basic_operations.h"
 
-#ifndef STATISTICS_H
-#define STATISTICS_H
-
 /**
-  @brief Calculate the arithmetic mean of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated mean.
-*/
+ * @brief Calculate the arithmetic mean of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated mean.
+ */
 
 double mean(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result,
       sum = 0;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
     sum += arr[i];
   result = sum / length;
@@ -27,20 +29,23 @@ double mean(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the weighted mean of a set of values.
-  @param valuesWeightsPairs The array of value-weight pairs. Each pair is represented
+ * @brief Calculate the weighted mean of a set of values.
+ *
+ * @param valuesWeightsPairs The array of value-weight pairs. Each pair is represented
   as a double array with two elements: [value, weight].
-  @param length The length of the valuesWeightsPairs array.
-  @return The weighted mean of the arr.
+ * @param length The length of the valuesWeightsPairs array.
+ *
+ * @return The weighted mean of the arr.
 */
 
 double weightedMean(double **valuesWeightsPairs, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result, value, weight,
       sum = 0,
       weightSum = 0;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
   {
     value = valuesWeightsPairs[i][0];
@@ -54,39 +59,50 @@ double weightedMean(double **valuesWeightsPairs, unsigned length)
 }
 
 /**
-  @brief Calculate the trimmed mean of an array of values.
-  @param arr The array of values.
-  @param length The length of the arr array.
-  @param trimmedMeanPercentage The percentage of elements to be trimmed from both ends.
-  @return The trimmed mean of the arr.
-*/
+ * @brief Calculate the trimmed mean of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the arr array.
+ * @param trimmedMeanPercentage The percentage of elements to be trimmed from both ends.
+ *
+ * @return The trimmed mean of the arr.
+ */
 
-double trimmedMean(double *arr, unsigned lenght, double trimmedMeanPercentage)
+double trimmedMean(double *arr, unsigned length, double trimmedMeanPercentage)
 {
-  unsigned nElementsToTrim = (unsigned)round(nPercentOfX(lenght, trimmedMeanPercentage));
-  assert(2 * nElementsToTrim < lenght);
-  arr = sort(arr, lenght);
+  if (length == 0)
+    return NAN;
+  unsigned nElementsToTrim = (unsigned)round(nPercentOfX(length, trimmedMeanPercentage));
+  if (2 * nElementsToTrim >= length)
+    return NAN;
+  double result, *sortedArr = sort(arr, length);
   arr += nElementsToTrim;
-  lenght -= 2 * nElementsToTrim;
-  return mean(arr, lenght);
+  length -= 2 * nElementsToTrim;
+  result = mean(arr, length);
+  free(sortedArr);
+  return result;
 }
 
 /**
-  @brief Calculate the geometric mean of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated geometric mean.
-*/
+ * @brief Calculate the geometric mean of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated geometric mean.
+ */
 
 double geometricMean(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result,
       product = 1;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
   {
-    assert(arr[i] >= 0);
+    if (arr[i] <= 0)
+      return NAN;
     product *= arr[i];
   }
   result = nthRoot(product, length);
@@ -94,21 +110,25 @@ double geometricMean(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the harmonic mean of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated harmonic mean.
-*/
+ * @brief Calculate the harmonic mean of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated harmonic mean.
+ */
 
 double harmonicMean(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result,
       sum = 0;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
   {
-    assert(arr[i] > 0);
+    if (arr[i] <= 0)
+      return NAN;
     sum += 1 / arr[i];
   }
   result = length / sum;
@@ -116,44 +136,55 @@ double harmonicMean(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the median of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated median.
-*/
+ * @brief Calculate the median of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated median.
+ */
 
 double median(double *arr, unsigned length)
 {
+  if (length <= 0)
+    return NAN;
   double result,
       *sortedArr = sort(arr, length);
-  assert(length > 0);
   if (length % 2)
     result = sortedArr[length / 2];
   else
     result = (sortedArr[length / 2 - 1] + sortedArr[length / 2]) / 2;
+  free(sortedArr);
   return result;
 }
 
 /**
-  @brief Calculate the mode(s) of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @param ptrNOfModes Pointer to store the number of modes found.
-  @return An array containing the mode(s) of the input values.
-  @note The number of modes is stored in ptrNOfModes.
-*/
+ * @brief Calculate the mode(s) of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ * @param ptrNOfModes Pointer to store the number of modes found.
+ *
+ * @return An array containing the mode(s) of the input values.
+ *
+ * @note The number of modes is stored in ptrNOfModes.
+ */
 
 double *mode(double *arr, unsigned length, unsigned *ptrNOfModes)
 {
-  double **frequency, *result, *sortedArr;
+  if (length == 0)
+  {
+    *ptrNOfModes = 0;
+    return NULL;
+  }
+  double **frequency, *result,
+      *sortedArr = sort(arr, length);
   unsigned i,
       minFreq = __UINT32_MAX__,
       maxFreq = 0,
       resultLenght = 0,
       frequencyLength = 0;
-  assert(length > 0);
   result = NULL;
-  sortedArr = sort(arr, length);
   frequency = (double **)malloc(sizeof(*frequency) * length);
   for (i = 0; i < length; i++)
     frequency[i] = (double *)calloc(sizeof(**frequency), 2);
@@ -179,52 +210,69 @@ double *mode(double *arr, unsigned length, unsigned *ptrNOfModes)
         result = (double *)realloc(result, sizeof(*result) * ++(*ptrNOfModes));
         result[resultLenght++] = frequency[i][0];
       }
+
+  for (i = 0; i < length; i++)
+    free(frequency[i]);
+
+  free(frequency);
+  free(sortedArr);
+
   return result;
 }
 
 /**
-  @brief Find the minimum value in an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The minimum value.
-*/
+ * @brief Find the minimum value in an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The minimum value.
+ */
 
 double min(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result = __DBL_MAX__;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
     result = arr[i] < result ? arr[i] : result;
   return result;
 }
 
 /**
-  @brief Find the maximum value in an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The maximum value.
-*/
+ * @brief Find the maximum value in an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The maximum value.
+ */
 
 double max(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result = -__DBL_MAX__;
   unsigned i;
-  assert(length > 0);
   for (i = 0; i < length; i++)
     result = arr[i] > result ? arr[i] : result;
   return result;
 }
 
 /**
-  @brief Calculate the range of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated range.
-*/
+ * @brief Calculate the range of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated range.
+ */
 
 double range(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double minValue = min(arr, length),
          maxValue = max(arr, length),
          result = maxValue - minValue;
@@ -232,11 +280,13 @@ double range(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the midrange of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated midrange.
-*/
+ * @brief Calculate the midrange of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated midrange.
+ */
 
 double midrange(double *arr, unsigned length)
 {
@@ -245,14 +295,18 @@ double midrange(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the variance of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated variance.
-*/
+ * @brief Calculate the variance of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated variance.
+ */
 
 double variance(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double mu = mean(arr, length),
          sum = 0,
          result;
@@ -264,32 +318,39 @@ double variance(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the standard deviation of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated standard deviation.
-*/
+ * @brief Calculate the standard deviation of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated standard deviation.
+ */
 
 double standardDeviation(double *arr, unsigned length)
 {
+  if (length == 0)
+    return NAN;
   double result = sqrt(variance(arr, length));
   return result;
 }
 
 /**
-  @brief Calculate the sample variance of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated sample variance.
-*/
+ * @brief Calculate the sample variance of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated sample variance.
+ */
 
 double sampleVariance(double *arr, unsigned length)
 {
+  if (length < 1)
+    return NAN;
   double mu = mean(arr, length),
          sum = 0,
          result;
   unsigned i;
-  assert(length != 1);
   for (i = 0; i < length; i++)
     sum += pow((arr[i] - mu), 2);
   result = sum / (length - 1);
@@ -297,14 +358,18 @@ double sampleVariance(double *arr, unsigned length)
 }
 
 /**
-  @brief Calculate the sample standard deviation of an array of values.
-  @param arr The array of values.
-  @param length The length of the array.
-  @return The calculated sample standard deviation.
-*/
+ * @brief Calculate the sample standard deviation of an array of values.
+ *
+ * @param arr The array of values.
+ * @param length The length of the array.
+ *
+ * @return The calculated sample standard deviation.
+ */
 
 double sampleStandardDeviation(double *arr, unsigned length)
 {
+  if (length < 1)
+    return NAN;
   double result = sqrt(sampleVariance(arr, length));
   return result;
 }
