beb1f91deb5d0613e40740534b5059ce8d3d1bec
amartinsmg <andremindurimg@gmail.com>
2026-08-10 11:33:24 -0300

    tests(memory leak): fix memory leak

--- tests/test_c.c
+++ tests/test_c.c
@@ -15,11 +15,14 @@ int main()
 {
   double arr[SIZE_OF_ARR] = {2, 4, 6, 7, 5, 3, 2, 1};
   size_t n_of_modes;
-  double* mode_result = mode(arr, SIZE_OF_ARR, &n_of_modes);
+  double* mode_ptr = mode(arr, SIZE_OF_ARR, &n_of_modes);
+  double mode_result = mode_ptr[0];
+
+  freeArray(mode_ptr);
 
   ASSERT_TRUE(mean(arr, SIZE_OF_ARR) == 3.75);
   ASSERT_TRUE(n_of_modes == 1);
-  ASSERT_TRUE(mode_result[0] == 2);
+  ASSERT_TRUE(mode_result == 2);
   ASSERT_TRUE(gcd(12, 42) == 6);
 
   printf("Sanity test passed!\n\n");
